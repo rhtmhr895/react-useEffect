276c367096c65ac0cd377b6540363fb58da7b10b
rohitsinghmehra-sdd <rohitsinghmehra@smartdatainc.net>
2023-01-17 10:44:57 +0530

    final commit

--- react_useeffect/src/Footer.js
+++ react_useeffect/src/Footer.js
@@ -1,7 +1,7 @@
 const Footer = ({ length }) => {
     return (
         <footer>
-            <p>{length} List {length === 1 ? "item" : "items"}</p>
+            <p>{length} List {length === 1 ? "item" : "items"} - Copyright &nbsp;</p>
         </footer>
     )
 }
